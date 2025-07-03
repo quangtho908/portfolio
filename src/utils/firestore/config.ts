01e466781a69794c38149f115d782c0702c7faa6
quangtho908 <quangtho23062002@gmail.com>
2025-07-03 12:53:02 +0700

    fix service account

--- src/utils/firestore/config.ts
+++ src/utils/firestore/config.ts
@@ -1,11 +1,15 @@
 import admin from "firebase-admin";
-
+import serviceAccount from "./service-account.json";
 const databaseURL = "https://default.firebaseio.com";
 
 export function getFirebaseAdmin() {
   if (!admin.apps.length) {
     admin.initializeApp({
-      credential: admin.credential.cert(`${process.cwd()}/service-account.json`),
+      credential: admin.credential.cert({
+        projectId: serviceAccount.project_id,
+        clientEmail: serviceAccount.client_email,
+        privateKey: serviceAccount.private_key,
+      }),
       databaseURL,
     });
   }
